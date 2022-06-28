68f4fc265aa3e1a2c2e9b559faf4016cb9579596
Ricardo <85304541+RichardDB7@users.noreply.github.com>
2022-06-27 21:40:38 -0300

    Create Break&Continue.java

---
+++ JAVA/Break&Continue.java
@@ -0,0 +1,15 @@
+    //PALABRAS BREAK & CONTINUE
+    
+		for( var contador = 0; contador < 3 ; contador++ ) {//Definimos la variable contador en la misma estructura iterativa ,su condicion de corte y el incremento
+			if( contador % 2 == 0) { //Verificamos si el numero que iteramos es par
+				System.out.println("contador =" + 	contador);
+				break;//Mediante el break terminamos el ciclo por lo que se imprime 0
+			}
+			
+			for( var contador = 0; contador < 3 ; contador++ ) {//Definimos la variable contador en la misma estructura iterativa ,su condicion de corte y el incremento
+				if( contador % 2 != 0) { //Si el numero es impar
+					continue; //Omite cualquier linea mas del ciclo for continuando a la siguiente iteracion
+					
+				}	
+			System.out.println("contador =" + 	contador);
+			}
