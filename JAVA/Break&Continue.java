3ff91b62022bb3a87234060a46a03981ff3e6b61
Ricardo <85304541+RichardDB7@users.noreply.github.com>
2022-06-27 21:46:06 -0300

    Update Break&Continue.java

--- JAVA/Break&Continue.java
+++ JAVA/Break&Continue.java
@@ -1,4 +1,4 @@
-    //PALABRAS BREAK & CONTINUE
+    		//PALABRAS BREAK & CONTINUE
     
 		for( var contador = 0; contador < 3 ; contador++ ) {//Definimos la variable contador en la misma estructura iterativa ,su condicion de corte y el incremento
 			if( contador % 2 == 0) { //Verificamos si el numero que iteramos es par
@@ -13,3 +13,14 @@
 				}	
 			System.out.println("contador =" + 	contador);
 			}
+	
+	
+		//ETIQUETAS O LABEL CONTINUE (PROGRAMACION "GO TO"-NO RECOMENDABLE-)
+			/*inicio:
+			for( var contador = 0; contador < 3 ; contador++ ) {//Definimos la variable en la misma estructura iterativa ,su condicion de corte y el incremento
+			if( contador % 2 != 0) { //Si el numero es impar
+				continue inicio; //Se indica al continue ir a la siguiente iteracion
+				
+			}	
+			System.out.println("contador =" + 	contador);
+			}*/
