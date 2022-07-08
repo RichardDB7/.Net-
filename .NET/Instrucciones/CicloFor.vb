84fb68674a9132faceb6bc575606d94e618d6a48
Ricardo <85304541+RichardDB7@users.noreply.github.com>
2022-07-08 10:36:25 -0300

    Create CicloFor.vb

---
+++ .NET/Instrucciones/CicloFor.vb
@@ -0,0 +1,13 @@
+/* Generar una instrucción For */
+
+Module Module1
+
+    Sub Main()
+        Dim i As Integer //Definimos la variable i como entera
+        For i = 0 To 4 // Incrementar de 0 a 4
+            Console.Write(i) //Imprimir cada vuelta del ciclo
+        Next // Finaliza el ciclo 
+
+    End Sub
+
+End Module
